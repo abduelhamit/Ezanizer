1406b9166178479d35a44e4fbd51889f8f466083
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2025-12-25 10:01:22 +0100

    Get credentials from environment variables

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("Starting Ezanizer...")
+	defer log.Println("Ezanizer stopped")
+
+	email, ok := os.LookupEnv("DIYANET_EMAIL")
+	if !ok || email == "" {
+		log.Fatalln("DIYANET_EMAIL is not set")
+	}
+
+	password, ok := os.LookupEnv("DIYANET_PASSWORD")
+	if !ok || password == "" {
+		log.Fatalln("DIYANET_PASSWORD is not set")
+	}
 }
