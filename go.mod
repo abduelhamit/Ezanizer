ca4d2ed4082f87e02b9f163a38c84c0f7fa3fc4a
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2025-12-26 21:33:55 +0100

    Retrieve country, state, and city data from API

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/abduelhamit/Ezanizer
 
 go 1.25.5
 
-require github.com/abduelhamit/DiyanetAwqatSalahAPI v0.0.0-20251226125541-7468727a484e
+require github.com/abduelhamit/DiyanetAwqatSalahAPI v0.0.0-20251226202958-3093409e5349
 
 require golang.org/x/oauth2 v0.34.0 // indirect
--- main.go
+++ main.go
@@ -24,13 +24,85 @@ func main() {
 		log.Fatalln("DIYANET_PASSWORD is not set")
 	}
 
+	countryCode, ok := os.LookupEnv("COUNTRY_CODE")
+	if !ok || countryCode == "" {
+		log.Fatalln("COUNTRY_CODE is not set")
+	}
+
+	stateCode, ok := os.LookupEnv("STATE_CODE")
+	if !ok || stateCode == "" {
+		log.Fatalln("STATE_CODE is not set")
+	}
+
+	cityCode, ok := os.LookupEnv("CITY_CODE")
+	if !ok || cityCode == "" {
+		log.Fatalln("CITY_CODE is not set")
+	}
+
 	config := &diyanet.Config{
 		Email:    email,
 		Password: password,
 	}
 
-	_, err := config.Token(ctx)
+	client := config.NewClient(ctx)
+	countries, err := client.GetCountries()
 	if err != nil {
-		log.Fatalf("Failed to get token: %v", err)
+		log.Fatalln("Failed to get countries:", err)
 	}
+
+	var country diyanet.Country
+	found := false
+	for _, c := range countries {
+		if c.Code == countryCode {
+			country = c
+			found = true
+			break
+		}
+	}
+	if !found {
+		log.Fatalf("country with code %q not found", countryCode)
+	}
+
+	states, err := client.GetStatesByCountry(country.Id)
+	if err != nil {
+		log.Fatalln("Failed to get states:", err)
+	}
+
+	var state diyanet.State
+	found = false
+	for _, s := range states {
+		if s.Code == stateCode {
+			state = s
+			found = true
+			break
+		}
+	}
+	if !found {
+		log.Fatalf("state with code %q not found", stateCode)
+	}
+
+	cities, err := client.GetCitiesByState(state.Id)
+	if err != nil {
+		log.Fatalln("Failed to get cities:", err)
+	}
+
+	var city diyanet.City
+	found = false
+	for _, c := range cities {
+		if c.Code == cityCode {
+			city = c
+			found = true
+			break
+		}
+	}
+	if !found {
+		log.Fatalf("city with code %q not found", cityCode)
+	}
+
+	cityDetail, err := client.GetCityDetail(city.Id)
+	if err != nil {
+		log.Fatalln("Failed to get city detail:", err)
+	}
+
+	log.Printf("City Detail: %+v\n", cityDetail)
 }
